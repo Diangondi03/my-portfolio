70768ca831cb0b1b6e55da9e9c8573b25e44e212
Diangondi03 <elguito2003@gmail.com>
2025-04-13 16:30:19 -0400

    Add educational timeline component with SVG assets and GIF logos
    - Implemented TimeLineElement component for displaying education items.
    - Created Education page to render a vertical timeline of educational experiences.
    - Added SVG and GIF assets for various educational institutions and courses.
    - Defined TypeScript declarations for image modules including PNG, JPG, JPEG, GIF, and SVG.

---
+++ src/pages/Education.tsx
@@ -0,0 +1,63 @@
+import { SchoolIcon, StarIcon, WorkflowIcon } from 'lucide-react';
+import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import '../../node_modules/react-vertical-timeline-component/style.min.css';
+import Universidad from "../assets/Universidad_Santa_María_logo.gif"
+import FreeCodeCamp from "../assets/freecodecamp-icon.svg"
+import UCV from "../assets/Logo_Universidad_Central_de_Venezuela.svg"
+import Udemy from "../assets/Udemy_Symbol.svg"
+import { EducationItem } from '../interfaces';
+import TimeLineElement from '../components/education/TimeLineElement';
+
+const EducationTimeLine : EducationItem[] = [
+    {
+        date: "2021 - 2026",
+        title: "Systems Engineering - Santa María University",
+        skills: "Systems Engineering, Software Development, Web Development, Mobile Development, Data Science",
+        icon: Universidad
+    },
+    {
+        date: "2025",
+        title: "Scientific Computing with Python - FreeCodeCamp",
+        skills: "Data Science, Machine Learning, Python Programming",
+        icon: FreeCodeCamp
+    },
+    {
+        date: "2024",
+        title: "Relational Database - FreeCodeCamp",
+        skills: "Database Design, SQL Programming",
+        icon: FreeCodeCamp
+    },
+    {
+        date: "2023 - 2024",
+        title: "Inglés (Adultos) como lengua extranjera - FUNDEIM",
+        skills: "English Language, Communication Skills",
+        icon: UCV
+    },
+    {
+        date: "2023",
+        title: "The Web Development Bootcamp 2023 - UDEMY",
+        skills: "Full Stack Web Development, HTML, CSS, JavaScript, Node.js, Express.js, MongoDB",
+        icon: Udemy
+    }
+]
+
+const Education = () => {
+  return (
+    <>
+        <h1 className='text-center text-3xl font-bold my-10'>Education</h1>
+        <VerticalTimeline>
+            
+            {EducationTimeLine.map((item, index) => (
+                <TimeLineElement
+                    key={index}
+                    item={item}
+                />
+            ))}
+            
+        </VerticalTimeline>
+    </>
+  )
+}
+
+export default Education
