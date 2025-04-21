9da89b872f2573a2861f48473b2be52a8ae0966a
Diangondi03 <elguito2003@gmail.com>
2025-04-20 22:21:47 -0400

    bg footer changed to gray gradient

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     const {t} = useContext(LanguageContext)
   return (
     <div className='h-[10vh]'>
-      <footer className='bg-black text-white p-4 text-center mt-10 h-[10vh] flex items-center justify-center flex-col absolute bottom-0 w-full'> 
+      <footer className='bg-gradient-to-r from-gray-950 via-gray-900 to-gray-950 text-white p-4 text-center mt-10 h-[10vh] flex items-center justify-center flex-col absolute bottom-0 w-full'>
       <p>&copy; {new Date().getFullYear()}{" "+t("footer:First")}</p>
       <p>
         {t("footer:Second")}<span className='text-blue-400'>React</span>.
